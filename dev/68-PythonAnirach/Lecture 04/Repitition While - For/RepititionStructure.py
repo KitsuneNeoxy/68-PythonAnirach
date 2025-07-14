38cecbff837731cf78706c1c1d308b3be80542bd
KitsuneNeoxy <1stXPoorinut@gmail.com>
2025-07-15 01:19:56 +0700

    commit CH4

---
+++ dev/68-PythonAnirach/Lecture 04/Repitition While - For/RepititionStructure.py
@@ -0,0 +1,18 @@
+sales = float(input('Enter the amount of sales: '))
+comm_rate = float(input('Enter the commission rate: '))
+commission = sales * comm_rate
+print('The commission is $',format(commission,',.2f'))
+
+sales = float(input('Enter the amount of sales: '))
+comm_rate = float(input('Enter the commission rate: '))
+commission = sales * comm_rate
+print('The commission is $',format(commission,',.2f'))
+
+sales = float(input('Enter the amount of sales: '))
+comm_rate = float(input('Enter the commission rate: '))
+commission = sales * comm_rate
+print('The commission is $',format(commission,',.2f'))
+
+'''
+Code นี้เป็น Sequence Structure ที่ยาวเเละเสียเวลา ทําให้เกินความจําเป็น
+'''
