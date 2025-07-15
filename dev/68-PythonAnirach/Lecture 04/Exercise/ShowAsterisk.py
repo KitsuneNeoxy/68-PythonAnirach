6801f6bfe895539692ad27afbda356e213609d4f
KitsuneNeoxy <1stXPoorinut@gmail.com>
2025-07-15 13:14:46 +0700

    update broh

--- dev/68-PythonAnirach/Lecture 04/Exercise/ShowAsterisk.py
+++ dev/68-PythonAnirach/Lecture 04/Exercise/ShowAsterisk.py
@@ -1,7 +1,5 @@
 Rows = int(input('How many rows: '))
 Columns = int(input('How many Columns: '))
 
-Result = Columns * '*'
-print(Result)
 for i in range(Rows):
-    print(Result)
+    print(Columns * '*')
