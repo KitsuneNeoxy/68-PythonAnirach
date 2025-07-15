9c5aa1460061647eac3c172999e0390dae867da4
KitsuneNeoxy <1stXPoorinut@gmail.com>
2025-07-15 16:20:40 +0700

    update

--- dev/68-PythonAnirach/Lecture 04/Exercise/Ex4.py
+++ dev/68-PythonAnirach/Lecture 04/Exercise/Ex4.py
@@ -2,8 +2,7 @@ chunk_Size = int(input('Enter chunk: '))
 numbers = ""
 
 for i in range(1,101):
-    numbers += str(i).rjust(3)
-
-for j in range(0, len(numbers),chunk_Size * 3):
-    print(numbers[j:j + chunk_Size * 3]) #J:J เป็นการ slice list
+    print(i,end=" ")
+    if i % chunk_Size == 0:
+        print('')
 
