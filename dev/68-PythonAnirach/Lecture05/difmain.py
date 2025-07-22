b18bbb154be3495dad6d3829b59c9ea1099ea22f
KitsuneNeoxy <1stXPoorinut@gmail.com>
2025-07-22 14:07:23 +0700

    huh

---
+++ dev/68-PythonAnirach/Lecture05/difmain.py
@@ -0,0 +1,9 @@
+def main():
+    print( 'have a message for you.')
+    message()
+    print('goodBye!')
+
+def message():
+    print('ควยไรไอสาสหน้าหีเย้ดหมั่ม')
+
+main()
---
+++ dev/68-PythonAnirach/Lecture05/message.py
@@ -0,0 +1,8 @@
+def message():
+    print("I'm Arthur")
+    
+    print("King's of the Britons")
+
+print("I've message for yah.")
+message()
+print('Goodbye')
